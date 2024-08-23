bee21fb3c092304a5c31b449be2b96af37579d7a
dubovac <dubovac@users.noreply.github.com>
2024-08-22 23:10:02 -0500

    Create 343.Integer_Break.py

---
+++ 343.Integer_Break.py
@@ -0,0 +1,59 @@
+# 343. Integer Break
+# Medium
+# Topics
+# Companies
+# Hint
+#
+# Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+#
+# Return the maximum product you can get.
+#
+#  
+#
+# Example 1:
+#
+#       Input: n = 2
+#       Output: 1
+#       Explanation: 2 = 1 + 1, 1 × 1 = 1.
+#
+# Example 2:
+#
+#       Input: n = 10
+#       Output: 36
+#       Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+#
+#  
+#
+# Constraints:
+#
+#       2 <= n <= 58
+
+def integerBreak(n):
+    if n == 2:
+        return 1
+
+    if n == 3:
+        return 2
+
+    if n == 4:
+        return 4
+
+    result = 1
+
+    while n > 4:
+        result *= 3
+        n -= 3
+
+    if n == 2:
+        result *= 2
+
+    if n == 3:
+        result *= 3
+
+    if n == 4:
+        result *= 4
+
+    return result
+
+for i in range(2, 59):
+    print(f"{i}:\t{integerBreak(i)}")
